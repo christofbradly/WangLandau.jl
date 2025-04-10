b50d3f1f7dcbfc585dbce020e18f9079515f2e45
christofbradly <christof.bradly@gmail.com>
2025-04-10 17:32:27 +1000

    ising example

--- docs/make.jl
+++ docs/make.jl
@@ -12,15 +12,17 @@ makedocs(;
         canonical="https://christofbradly.github.io/WangLandau.jl",
         edit_link="main",
         assets=String[],
+        size_threshold=500_000, # 500 kB
+        size_threshold_warn=200_000, # 200 kB
     ),
     pages=[
         "Home" => "index.md",
-        "Example: Ising model" => "ising.md",
+        "Example: 2D Ising model" => "ising.md",
         "Advanced usage" => "advanced.md",
         "API" => "api.md"
     ],
     # checkdocs=:exports,
-    doctest=false, # Doctests are done while testing.
+    # doctest=false, # Doctests are done while testing.
 )
 
 deploydocs(;
