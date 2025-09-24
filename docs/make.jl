c68fe967b4a851cb270015ba92bdb1d19ba9b0c9
Hellwoodoo <edward.xxu@gmail.com>
2025-09-24 21:05:59 +1000

    Fixing make.jl and removed additional files

--- docs/make.jl
+++ docs/make.jl
@@ -5,13 +5,13 @@ makedocs(;
     modules=[WangLandau],
     authors="Chris Bradly",
     sitename="WangLandau.jl",
-    # format=Documenter.HTML(;
-    #     canonical="https://christofbradly.github.io/WangLandau.jl",
-    #     edit_link="main",
-    #     assets=String[],
-    #     size_threshold=500_000, # 500 kB
-    #     size_threshold_warn=200_000, # 200 kB
-    # ),
+    format=Documenter.HTML(;
+        canonical="https://christofbradly.github.io/WangLandau.jl",
+        edit_link="main",
+        assets=String[],
+        size_threshold=500_000, # 500 kB
+        size_threshold_warn=200_000, # 200 kB
+    ),
     pages=[
         "Home" => "index.md",
         "Example: 2D Ising model" => "ising.md",
@@ -23,5 +23,6 @@ makedocs(;
 )
 
 deploydocs(
-    repo="github.com/christofbradly/WangLandau.jl"
+    repo="github.com/christofbradly/WangLandau.jl",
+    push_preview = true
 )
