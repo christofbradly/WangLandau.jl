c880b16b63289a02df36cae8a84bd23e578f0cc0
Hellwoodoo <edward.xxu@gmail.com>
2025-11-16 22:23:03 +1100

    Fixing

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,40 +6,31 @@ include(joinpath(@__DIR__, "..", "examples", "ising.jl"))
 
 function make_sim(L::Int; kwargs...)
     Random.seed!(12345)
-
     periodic = false
     statedefn = Ising2D(L; periodic)
     problem = WangLandauProblem(statedefn)
-
-    sim = solve(problem;
-        check_sweeps=check_sweeps,
-        final_logf=final_logf,
-        tasks_per_thread=tasks_per_thread,
-    )
-
-    return sim
+    return solve(problem; kwargs...)
 end
 
 @info "Benchmark running with $(Threads.nthreads()) threads"
 
 const SUITE = @benchmarkset "WangLandau" begin
     @benchmarkset "Scaling" begin
-        @case "L=16" begin
-            make_sim(16, check_sweeps=500, final_logf=1e-3)
+       @case "L=16" begin
+            make_sim(16; check_sweeps=500, final_logf=1e-3)
         end seconds=60
-
         @case "L=32" begin
-            make_sim(32, check_sweeps=1000, final_logf=1e-3)
+            make_sim(32; check_sweeps=1_000, final_logf=1e-3)
         end seconds=60
     end
 
     @benchmarkset "Threading" begin
         @case "L=16" begin
-            make_sim(16, check_sweeps=1000, final_logf=1e-3)
+            make_sim(16; check_sweeps=1_000, final_logf=1e-3)
         end seconds=60
 
         @case "L=32" begin
-            make_sim(32, check_sweeps=1000, final_logf=1e-3)
+            make_sim(32; check_sweeps=1_000, final_logf=1e-3)
         end seconds=60
     end
 end
