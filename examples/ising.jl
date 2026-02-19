2ce57d42558b0453be4a10d43c6ca490bb9991fc
Chris Bradly <cjbradly@proton.me>
2026-02-19 13:18:03 +1100

    println debugging

--- examples/ising.jl
+++ examples/ising.jl
@@ -107,6 +107,7 @@ function WangLandau.random_trial!(_, statedefn::Ising2D)
 end
 
 function WangLandau.histogram_index(spins, statedefn::Ising2D{Periodic}, site, old_index) where {Periodic}
+    println("inside ising.jl/histogram_index: ", site)
     if Periodic
         ΔE = local_energy_periodic(spins, site) ÷ 2
     else
